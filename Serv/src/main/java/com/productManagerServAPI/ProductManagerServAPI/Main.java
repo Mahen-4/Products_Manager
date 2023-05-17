621f06ecada377f952160099340565df6a3bd123
Mahen-4 <mahen.ath@outlook.com>
2023-05-17 17:11:26 +0200

    Initial Backend Commit

--- Serv/src/main/java/com/productManagerServAPI/ProductManagerServAPI/Main.java
+++ Serv/src/main/java/com/productManagerServAPI/ProductManagerServAPI/Main.java
@@ -13,21 +13,7 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/")
-    public HelloJSON hello(){
-         HelloJSON response = new HelloJSON(
-                "hello JAVA !",
-                List.of("JS","JS","JS"),
-                new Person("i SAID JS",12)
-                );
-        return response;
-    }
-    record Person(String name, int age){}
-    record HelloJSON(
-            String hello,
-            List<String> favProgrammingLang,
-            Person person
-    ){
+    {
 
     }
 }
